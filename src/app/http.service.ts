049ce7b3d3a15a553b1f7d3600ac5a81fbda746e
akilezhil <akilanms1999@gmail.com>
2021-08-17 15:37:24 +0530

    add https

--- src/app/http.service.ts
+++ src/app/http.service.ts
@@ -9,7 +9,7 @@ export class HttpService {
   constructor(private http: HttpClient) { }
 
   LoadData(value:any) {
-     return this.http.get('http://apilayer.net/api/validate?access_key=d9c52c285f7b3937ed0288332402b6ac&number='+value.mobileNumber+'&country_code='+value.country);
+     return this.http.get('https://apilayer.net/api/validate?access_key=d9c52c285f7b3937ed0288332402b6ac&number='+value.mobileNumber+'&country_code='+value.country);
   }
 
 }
